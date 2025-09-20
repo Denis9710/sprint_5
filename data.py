be06fa4d9e07ff1c56ce2391516e3f125c3b039f
Denis Balashov <tea-shop62@yandex.com>
2025-09-21 01:05:35 +0300

    My proekt sprint5

---
+++ data.py
@@ -0,0 +1,19 @@
+import random
+
+
+class DATA:
+    name = 'Денис'
+    password = '413121'
+    Email = 'denis_balashov_31_197@yandex.ru'
+    
+    @staticmethod
+    def generate_email():
+        return 'denis_balashov_31'+ str(random.randint(100,999)) + '@yandex.ru'
+
+    @staticmethod
+    def generate_password():
+        return str(random.randint(100000,999999))
+    @staticmethod
+    def generate_name():
+        names = ['Вадим', 'Сергей', 'Илья', 'Константин', 'Алексей', 'Равшан', 'Джамшут']
+        return random.choice(names)
