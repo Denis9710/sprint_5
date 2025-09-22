934e46fd4ae593dfd312d363ad5dd57f19daee6f
Denis Balashov <tea-shop62@yandex.com>
2025-09-23 00:19:49 +0300

    My proekt sprint5

--- data.py
+++ data.py
@@ -1,6 +1,21 @@
 import random
 
 class DATA:
+
+    #ссылка главной страницы 
+    current_url_home_page = 'https://stellarburgers.nomoreparties.site/' 
+    #ссылка ленты заказов 
+    current_url_order_feed = current_url_home_page + 'feed'
+    #ссылка формы регестрации
+    current_url_form_signup = current_url_home_page + 'register'
+    #ссылка формы восстановления пароля 
+    current_url_recovery_password = current_url_home_page + 'forgot-password'
+    #ссылка формы входа
+    current_url_entrance = current_url_home_page + 'login'
+    #ссылка личного кабинета
+    current_url_personal_account = current_url_home_page + 'account/profile'
+
+
     name = 'Денис'
     password = '413121'
     Email = 'denis_balashov_31_197@yandex.ru'
--- locators.py
+++ locators.py
@@ -1,67 +1,54 @@
 from selenium.webdriver.common.by import By
 
-#Текущие ссылки
-class URL:
-    # Форма входа на сайт бургерной
-    current_url_entrance = 'https://stellarburgers.nomoreparties.site/login'
-    # Главная страница бургерной
-    current_url_home_page = 'https://stellarburgers.nomoreparties.site/' 
-    # Переход в личный кабинет
-    current_url_personal_account = 'https://stellarburgers.nomoreparties.site/account/profile'
-    # Лента заказов бургерной
-    current_url_order_feed = 'https://stellarburgers.nomoreparties.site/feed'
-    # Регистрация на сайте 
-    current_url_form_signup = 'https://stellarburgers.nomoreparties.site/register'
-    # Восстановить пароль на сайте 
-    current_url_recovery_password = 'https://stellarburgers.nomoreparties.site/forgot-password'
 
-
-
-# Кнопки которые находятся на главной странице бургерной
+# Кнопки на главной странице
 class BUTTON:
-    # Кнопка "логотип"
-    logo_button = (By.XPATH, '//a[@href="/"]') 
-    # Кнопка "войти в аккаунт"
-    entrance_in_account = (By.XPATH, './/button[text() = "Войти в аккаунт"]')       
-    # Кнопка "личного кабинета"
+    # Кнопка Личный Кабинет
     personal_account_button = (By.XPATH, './/p[text() = "Личный Кабинет"]')
-    # Кнопка "конструктор"
-    constructor_button = (By.XPATH, '//p[text() = "Конструктор"]')
-    # Кнопка "оформить заказ"
-    place_an_order = (By.XPATH, './/button[text() = "Оформить заказ"]')
-    # Кнопка "лента заказов"
+    # Кнопка логотип
+    logo_button = (By.XPATH, '//a[@href="/"]')
+    # Кнопка Лента заказов
     order_feed_button = (By.XPATH, '//p[text() = "Лента Заказов"]')
-    # Кнопка "выйти из аккаунт"
+    #Кнопка конструктор
+    constructor_button = (By.XPATH, '//p[text() = "Конструктор"]')
+    #Кнопка "Войти в аккаунт"
+    entrance_in_account = (By.XPATH, './/button[text() = "Войти в аккаунт"]')
+    #Кнопка "Выйти из аккаунт"
     exit_from_account = (By.XPATH, './/button[text() = "Выход"]')
+    #Кнопка "Оформить заказ"
+    place_an_order = (By.XPATH, './/button[text() = "Оформить заказ"]')
 
-# Форма регистрации на сайте бургерной
+
+# Форма входа 
+class ENTRANCE:
+    # Поле email
+    field_email = (By.XPATH, '//input[@name="name"]')
+    field_password = (By.XPATH, './/h2[text() = "Вход"]/following::input[@type="password"]')
+    # Кнопка войти
+    entrance_button1 = (By.XPATH, './/button[@class = "button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa" and text() = "Войти"]')
+    # Кнопка зарегестрироваться
+    signup = (By.XPATH, './/a[@class="Auth_link__1fOlj" and text()= "Зарегистрироваться"]')
+    # Кнопка Восстановить пароль
+    restore_password = (By.XPATH, './/a[@class="Auth_link__1fOlj" and text() = "Восстановить пароль"]')
+    # Текст "Некорректный пароль"
+    invalid_password = (By.XPATH, './/p[text() = "Некорректный пароль"]')
+
+
+# Форма регестрации
 class SIGNUP:
-    # Поле "Имя"
+    # Поле имя
     field_name = (By.XPATH, './/label[text() = "Имя"]/following-sibling::input')
-    # Поле "email"
+    # Поле email
     field_email = (By.XPATH, './/label[text() = "Email"]/following-sibling::input')
-    # Поле "Пароль"
+    # Поле пароль 
     field_password2 = (By.XPATH, './/label[text() = "Пароль"]/following-sibling::input')
-    # Кнопка "Зарегестрироваться"
+    # Кнопка Зарегестрироваться
     button_signup = (By.XPATH, './/form//button[text()="Зарегистрироваться"]')
-    # Кнопка "Войти"
+    # Кнопка Войти
     entrance_button2 = (By.XPATH, './/a[@class="Auth_link__1fOlj" and text()= "Войти"]')
     # Надпись при дублирование данных при регестрации 
     text_during_dubbing = (By.XPATH, './/p[text()= "Такой пользователь уже существует"]')
 
-# Форма входа на сайт бургерной
-class ENTRANCE:
-    # Поле "email"
-    field_email = (By.XPATH, '//input[@name="name"]')
-    field_password = (By.XPATH, './/h2[text() = "Вход"]/following::input[@type="password"]')
-    # Кнопка "войти"
-    entrance_button1 = (By.XPATH, './/button[@class = "button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa" and text() = "Войти"]')
-    # Кнопка "зарегестрироваться"
-    signup = (By.XPATH, './/a[@class="Auth_link__1fOlj" and text()= "Зарегистрироваться"]')
-    # Кнопка "восстановить пароль"
-    restore_password = (By.XPATH, './/a[@class="Auth_link__1fOlj" and text() = "Восстановить пароль"]')
-    # Текст "некорректный пароль"
-    invalid_password = (By.XPATH, './/p[text() = "Некорректный пароль"]')
 
 # Форма восстановления пароля 
 class RESTORE:
@@ -72,10 +59,10 @@ class RESTORE:
 
 #Конструктор 
 class KON:
-    # Булки
+    #Кнопка булки
     button_buns = (By.XPATH, '//span[text()="Булки"]')
-    # Соусы
+    #Кнопка соусы
     button_sauces = (By.XPATH, '//span[text()="Соусы"]')
-    # Начинка
+    #Кнопка начинка
     button_fillings = (By.XPATH, '//span[text()="Начинки"]')
     active_tabl =  (By.CSS_SELECTOR, ".tab_tab_type_current__2BEPc")
